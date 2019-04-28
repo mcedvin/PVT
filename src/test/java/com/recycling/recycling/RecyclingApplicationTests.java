9413d786c4c9dd37e8a0567d91bb5e0dae673a09
mcedvin <edvin.of@gmail.com>
2019-04-28 17:41:26 +0200

    test

--- src/test/java/com/recycling/recycling/RecyclingApplicationTests.java
+++ src/test/java/com/recycling/recycling/RecyclingApplicationTests.java
@@ -1,16 +1,16 @@
-package com.recycling.recycling;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RecyclingApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.recycling.recycling;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class RecyclingApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
