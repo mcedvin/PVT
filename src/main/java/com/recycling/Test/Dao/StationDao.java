09560e14464890e94ab00d5ce4a99b98cb2d7f06
mcedvin <edvin.of@gmail.com>
2019-04-28 17:22:05 +0200

    test

---
+++ src/main/java/com/recycling/Test/Dao/StationDao.java
@@ -0,0 +1,30 @@
+package com.recycling.Test.Dao;
+
+import com.recycling.recycling.JSON.JSONparsing;
+import com.recycling.recycling.production.Position;
+import com.recycling.recycling.production.Station;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class StationDao {
+    private Map<Integer, Station> stations = new JSONparsing().getAllStations();
+
+//    static {
+//        stations = new HashMap<Integer, Station>() {
+//            {
+//                put(1, new Station("årstaviken", new Position(1,1)));
+//                put(2, new Station("gullmars", new Position(2,2)));
+//                put(3, new Station("årstaviken", new Position(3,3)));
+//
+//            }
+//        };
+//    }
+
+    public Collection<Station> getAllStations() {
+        return this.stations.values();
+    }
+}
---
+++ src/main/java/com/recycling/Test/Service/StationService.java
@@ -0,0 +1,19 @@
+package com.recycling.Test.Service;
+
+import com.recycling.Test.Dao.StationDao;
+import com.recycling.recycling.production.Station;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StationService {
+
+    @Autowired
+    private StationDao stationDao;
+
+    public Collection<Station> getAllStations() {
+        return stationDao.getAllStations();
+    }
+}
