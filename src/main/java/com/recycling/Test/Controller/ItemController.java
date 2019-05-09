2d334a83b1e153ca1d90a6404aa372e62360bb76
mcedvin <edvin.of@gmail.com>
2019-05-09 18:11:50 +0200

    SQL-integration

--- src/main/java/com/recycling/Test/Controller/ItemController.java
+++ src/main/java/com/recycling/Test/Controller/ItemController.java
@@ -1,41 +1,41 @@
-package com.recycling.Test.Controller;
-
-
-import com.recycling.Test.Service.ItemService;
-import com.recycling.recycling.production.Item;
-import com.recycling.recycling.production.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
-@RestController
-@RequestMapping("/items")
-public class ItemController {
-
-    @Autowired
-    private ItemService ItemService;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Collection<Item> getAllItems() {
-        return ItemService.getAllItems();
-    }
-
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addItem(@RequestBody final Item item) {
-        ItemService.addItem(item);
-    }
-
-//    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void updateUser(@RequestBody User user) {
-//        ItemService.updateUser(user);
+//package com.recycling.Test.Controller;
+//
+//
+//import com.recycling.Test.Service.ItemService;
+//import com.recycling.recycling.production.Item;
+//import com.recycling.recycling.production.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Collection;
+//@RestController
+//@RequestMapping("/items")
+//public class ItemController {
+//
+//    @Autowired
+//    private ItemService ItemService;
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    public Collection<Item> getAllItems() {
+//        return ItemService.getAllItems();
 //    }
 //
 //    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void addUser(@RequestBody User user) {
-//        userService.addUser(user);
+//    public void addItem(@RequestBody final Item item) {
+//        ItemService.addItem(item);
 //    }
-}
+//
+////    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+////    public void updateUser(@RequestBody User user) {
+////        ItemService.updateUser(user);
+////    }
+////
+////    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+////    public void addUser(@RequestBody User user) {
+////        userService.addUser(user);
+////    }
+//}
--- src/main/java/com/recycling/Test/Controller/StationController.java
+++ src/main/java/com/recycling/Test/Controller/StationController.java
@@ -1,23 +1,23 @@
-package com.recycling.Test.Controller;
-
-
-import com.recycling.Test.Service.StationService;
-import com.recycling.recycling.production.Station;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
-@RestController
-@RequestMapping("/stations")
-public class StationController {
-
-    @Autowired
-    private StationService stationService;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Collection<Station> getAllStations() {
-        return stationService.getAllStations();
-    }
-}
+//package com.recycling.Test.Controller;
+//
+//
+//import com.recycling.Test.Service.StationService;
+//import com.recycling.recycling.production.Station;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Collection;
+//@RestController
+//@RequestMapping("/stations")
+//public class StationController {
+//
+//    @Autowired
+//    private StationService stationService;
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    public Collection<Station> getAllStations() {
+//        return stationService.getAllStations();
+//    }
+//}
