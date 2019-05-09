1660be1ad5d095cdd0e9f4357bab7f1b2ea24ae5
mcedvin <edvin.of@gmail.com>
2019-05-09 18:30:37 +0200

    SQL-integration

--- src/main/java/com/recycling/Test/Controller/PositionController.java
+++ src/main/java/com/recycling/Test/Controller/PositionController.java
@@ -1,53 +1,53 @@
-//package com.recycling.Test.Controller;
-//
-//
-//import com.recycling.Test.Service.PositionService;
-//import com.recycling.recycling.production.Position;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Collection;
-//import java.util.List;
-//
-////@Controller
-//@RestController
-//@RequestMapping("/positions")
-//public class PositionController {
-//
-//    @Autowired
-//    private PositionService PositionService;
-//
-//    //        @GetMapping(value = "/all")
-//    @RequestMapping(method = RequestMethod.GET)
-//    public Collection<Position> getAllPositions() {
-//        return PositionService.getAllPositions();
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-//    public Position getPositionById(@PathVariable("id") int id) {
-//        return PositionService.getPositionById(id);
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-//    public void deletePositionById(@PathVariable("id") int id) {
-//        PositionService.removePositionById(id);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void updatePosition(@RequestBody Position Position) {
-//        PositionService.updatePosition(Position);
-//    }
-//
-//////        @PostMapping(value = "/load")
-////    public void addPosition(@RequestBody Position Position) {
-////        PositionService.addPosition(Position);
-////    }
-//
-//    //    @PostMapping(value = "/load")
-//    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void addPosition(@RequestBody final Position Position) {
+package com.recycling.Test.Controller;
+
+
+import com.recycling.Test.Service.PositionService;
+import com.recycling.recycling.production.Position;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+//@Controller
+@RestController
+@RequestMapping("/positions")
+public class PositionController {
+
+    @Autowired
+    private PositionService PositionService;
+
+    //        @GetMapping(value = "/all")
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Position> getAllPositions() {
+        return PositionService.getAllPositions();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Position getPositionById(@PathVariable("id") int id) {
+        return PositionService.getPositionById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletePositionById(@PathVariable("id") int id) {
+        PositionService.removePositionById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updatePosition(@RequestBody Position Position) {
+        PositionService.updatePosition(Position);
+    }
+
+////        @PostMapping(value = "/load")
+//    public void addPosition(@RequestBody Position Position) {
 //        PositionService.addPosition(Position);
 //    }
-//}
+
+    //    @PostMapping(value = "/load")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addPosition(@RequestBody final Position Position) {
+        PositionService.addPosition(Position);
+    }
+}
--- src/main/java/com/recycling/Test/Controller/UserController.java
+++ src/main/java/com/recycling/Test/Controller/UserController.java
@@ -1,52 +1,52 @@
-//package com.recycling.Test.Controller;
-//
-//
-//import com.recycling.Test.Service.UserService;
-//import com.recycling.recycling.production.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Collection;
-//import java.util.List;
-//
-////@Controller
-//@RestController
-//@RequestMapping("/users")
-//public class UserController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-////        @GetMapping(value = "/all")
-//    @RequestMapping(method = RequestMethod.GET)
-//    public Collection<User> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-//    public User getUserById(@PathVariable("id") int id) {
-//        return userService.getUserById(id);
-//    }
-//
-//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-//    public void deleteUserById(@PathVariable("id") int id) {
-//        userService.removeUserById(id);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void updateUser(@RequestBody User user) {
-//        userService.updateUser(user);
-//    }
-//
-//////        @PostMapping(value = "/load")
-////    public void addUser(@RequestBody User user) {
-////        userService.addUser(user);
-////    }
-//
-////    @PostMapping(value = "/load")
-//    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void addUser(@RequestBody final User user) {
+package com.recycling.Test.Controller;
+
+
+import com.recycling.Test.Service.UserService;
+import com.recycling.recycling.production.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+//@Controller
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+//        @GetMapping(value = "/all")
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable("id") int id) {
+        return userService.getUserById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteUserById(@PathVariable("id") int id) {
+        userService.removeUserById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+    }
+
+////        @PostMapping(value = "/load")
+//    public void addUser(@RequestBody User user) {
 //        userService.addUser(user);
 //    }
-//}
+
+//    @PostMapping(value = "/load")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addUser(@RequestBody final User user) {
+        userService.addUser(user);
+    }
+}
